79711095457f740d386a4f6ca1b7d97225827fc9
Shuntaro Sato <shuntaro@ShuntaronoMacBook-Air.local>
2023-02-17 15:56:55 +0900

    change

--- 04_Script/icd10jp.R
+++ 04_Script/icd10jp.R
@@ -38,7 +38,6 @@ df03_icd10 <- df02_icd10 |>
   mutate(code_icd9 = icd_map(code_icd10_p, from = 10, to = 9)) |> 
   select(cat_middle, cat_3, code_icd10_p, code_icd10, codename_high, codename, code_icd9)
 
-
 # Output ------------------------------------------------------------------
 
 write_rds(df03_icd10, "03_Output/icd10code.rds")
